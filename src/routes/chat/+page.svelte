f107d4242a0da685c0eb16e8d1ba173fc16d2529
codenius <94353572+codenius@users.noreply.github.com>
2022-09-08 16:29:10 +0000

    Store messages client side over page reload

--- src/routes/chat/+page.svelte
+++ src/routes/chat/+page.svelte
@@ -1,11 +1,19 @@
 <script>
-    import { onMount } from 'svelte';
-    import { io } from 'socket.io-client';
+    import { onMount } from "svelte";
+    import { io } from "socket.io-client";
     const socket = io();
 
-    let user, username, msgDraft, messages = [];
+    let user,
+        username,
+        msgDraft,
+        messages = [],
+        ready;
 
-    onMount(() => { user = localStorage.user; })
+    onMount(() => {
+        user = localStorage.user;
+        messages = JSON.parse(localStorage.messages || "[]");
+        ready = true;
+    });
 
     function login() {
         user = username;
@@ -14,37 +22,45 @@
 
     function logout() {
         user = undefined;
+        localStorage.removeItem("user");
+        localStorage.removeItem("messages");
     }
 
     function sendMessage() {
-        socket.emit("message", {text: msgDraft, user});
+        socket.emit("message", { text: msgDraft, user });
         msgDraft = "";
     }
 
     socket.on("message", (message) => {
         // Listen to the message event
         messages = [...messages, message];
+        localStorage.messages = JSON.stringify(messages);
     });
 </script>
 
 <main>
-    {#if user}
-        <span>{user}</span><button on:click={logout}>Logout</button>
-        <ul>
-            {#each messages as msg}
-                <li><div>{msg.user} at {msg.time}</div><div>{msg.text}</div></li>
-            {/each}
-        </ul>
-        <form on:submit|preventDefault={sendMessage}>
-            <input autofocus bind:value={msgDraft} type="text" /><button type="submit"
-                >Send</button
-            >
-        </form>
-    {:else}
-        <form on:submit|preventDefault={login}>
-            <input bind:value={username} type="text" /><button type="submit"
-                >Login</button
-            >
-        </form>
+    {#if ready}
+        {#if user}
+            <span>{user}</span><button on:click={logout}>Logout</button>
+            <ul>
+                {#each messages as msg}
+                    <li>
+                        <div>{msg.user} at {msg.time}</div>
+                        <div>{msg.text}</div>
+                    </li>
+                {/each}
+            </ul>
+            <form on:submit|preventDefault={sendMessage}>
+                <input autofocus bind:value={msgDraft} type="text" /><button
+                    type="submit">Send</button
+                >
+            </form>
+        {:else}
+            <form on:submit|preventDefault={login}>
+                <input bind:value={username} type="text" /><button type="submit"
+                    >Login</button
+                >
+            </form>
+        {/if}
     {/if}
 </main>
